3d3f356b3320f6e78e3a2c1ecb6af933a06a91e8
Jwiggs27 <89150842+Jwiggs27@users.noreply.github.com>
2022-01-18 11:07:18 -0600

    changed text and background

--- Unit1/hello/sketch.js
+++ Unit1/hello/sketch.js
@@ -4,8 +4,8 @@ function setup() {
 }
 
 function draw() {
-  background('blue') ;
+  background('orange') ;
   fill('white') ;
   textSize(32) ;
-  text("hello world!", 250, 250) ;
+  text("PopTarts", 250, 250) ;
 }
